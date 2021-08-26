20b539b6a7903cb4343f9175c79d720cf0d77506
Mina Anne Christensen <annechristensen88@live.dk>
2021-08-26 13:11:32 +0200

    DI

--- Mac.PetShop2021comp1.UI/Program.cs
+++ Mac.PetShop2021comp1.UI/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Security.Authentication.ExtendedProtection;
 using Mac.PetShop2021comp.Domain.IRepositories;
 using Mac.PetShop2021comp.Domain.Services;
 using Mac.PetShop2021comp1.Core.IServices;
 using Mac.PetShop2021comp1.Core.Models;
 using Mac.PetShop2021comp1.Infrastructure.DataAcces.Repositories;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mac.PetShop2021comp1.UI
 {
@@ -11,10 +13,17 @@ namespace Mac.PetShop2021comp1.UI
     {
         static void Main(string[] args)
         {
-            IPetRepository repo = new PetRepositoryInMemory();
-            IPetService service = new PetService(repo);
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<IPetRepository, PetRepositoryInMemory>();
+            serviceCollection.AddScoped<IPetService, PetService>();
 
-            var menu = new Menu(service);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var petService = serviceProvider.GetRequiredService<IPetService>();
+
+            //IPetRepository repo = new PetRepositoryInMemory();
+            //IPetService service = new PetService(repo);
+
+            var menu = new Menu(petService);
             menu.Start();
         }
     }
