2401fba6812266110c584920e0d2591fca78ea55
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2021-10-12 10:50:28 +0200

    security

---
+++ MySecurity/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MySecurity.Entities
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Role { get; set; }
+    }
+}
